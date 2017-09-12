cf50cd47aa7cb0e30409596b3638601726d3cf83
Lenny Peters <lpeters@hollandamericagroup.com>
2017-09-12 12:11:33 -0700

    master - test

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ var request = require("supertest");
 var app = require("../app.js");
 
 describe("GET /", function () {
-    it("respond with hello lenny", function (done){
-        request(app).get("/").expect("hello lenny", done);
+    it("respond with hello sundeep", function (done){
+        request(app).get("/").expect("hello sundeep", done);
     });
 });
 
