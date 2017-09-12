62628cf340a031cc68bd16335b7c4c78eb32bc02
Lenny Peters <lpeters@hollandamericagroup.com>
2017-09-12 12:44:40 -0700

    Master - fixed it

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@ var request = require("supertest");
 var app = require("../app.js");
 
 describe("GET /", function () {
-    it("respond with hello darin", function (done){
-        request(app).get("/").expect("hello darin", done);
+    it("respond with hello sundeep", function (done){
+        request(app).get("/").expect("hello sundeep", done);
     });
 });
 
